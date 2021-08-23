e0dd4ccf69eaf18a2aebc8ff4b415129d1a831e1
skelviper <skelviper@hotmail.com>
2021-08-23 21:44:39 +0800

    debug on runCHARM.sh

--- runCHARM.sh
+++ runCHARM.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#usage: ./runCHARM.sh projectName
+#usage: ./runCHARM.sh
 
 cd ../
 mkdir -p slurm_log
 snakemake --cluster 'sbatch --output=slurm_log/slurm-%j.out --cpus-per-task={threads} -t 7-00:00 -J CHARM!' --jobs 188 --resources nodes=188 --rerun-incomplete -s ./CHARM/CHARM.smk --keep-going
 
 mkdir -p ./analysis
-cp CHARM/stat.ipynb ./analysis/$1.stat.ipynb
+cp CHARM/stat.ipynb ./analysis/stat.ipynb
