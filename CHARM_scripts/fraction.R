7b1578c7c6450eebad057f283e4058658b9dd912
skelviper <skelviper@hotmail.com>
2021-09-04 23:39:07 +0800

    upgratetoV0.2

--- CHARM_scripts/fraction.R
+++ CHARM_scripts/fraction.R
@@ -7,12 +7,16 @@ if (length(args)==0) {
 
 
 library(tidyverse)
-countMatrix<- read_table2(args[1])
+countMatrix<- read_table2(args[1]) %>% select(-X2)
 
-countMatrix <- countMatrix %>% mutate(overlapTimes = str_count(gene,",")+1) %>% select(gene,overlapTimes,everything())
+if(dim(countMatrix)[2]<2){
+    write_tsv(x=countMatrix,path=args[2])
+} else {
+    countMatrix <- countMatrix %>% mutate(overlapTimes = str_count(gene,",")+1) %>% select(gene,overlapTimes,everything())
 
-datNormed <-  lapply(countMatrix %>% select(-gene), function(x) x/countMatrix[[2]])
-datNormed <- cbind(countMatrix[1],datNormed) %>% select(-overlapTimes)
-datNormed <- datNormed %>% mutate(gene = strsplit(gene,",")) %>% unnest(gene) %>% group_by(gene) %>% summarise_each(funs(sum))
-                     
-write_tsv(x = datNormed,path = args[2])
+    datNormed <-  lapply(countMatrix %>% select(-gene), function(x) x/countMatrix[[2]])
+    datNormed <- cbind(countMatrix[1],datNormed) %>% select(-overlapTimes)
+    datNormed <- datNormed %>% mutate(gene = strsplit(gene,",")) %>% unnest(gene) %>% group_by(gene) %>% summarise_each(funs(sum))
+
+    write_tsv(x = datNormed,path = args[2])
+}
